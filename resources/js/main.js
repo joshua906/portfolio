4fc33b4026f9b3b775386e5027e500b61c3f3af3
joshua906 <joshuamatthews906@yahoo.com>
2016-12-30 17:14:43 -0600

    menu channges

--- resources/js/main.js
+++ resources/js/main.js
@@ -50,6 +50,13 @@ jQuery(document).ready(function($){
    		$(this).toggleClass('active');
 		$('#overlay').toggleClass('open');
   	});
-	
+	//colum grid
+			$('.grid').masonry({
+		  // set itemSelector so .grid-sizer is not used in layout
+		  itemSelector: '.grid-item',
+		  // use element for option
+		  columnWidth: '.grid-sizer',
+		  percentPosition: true
+		})
 	
 });
